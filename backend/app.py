7a897471689306cc3a9d69554ee39be1dccd1517
Prabhjot Singh <61862441+lifewhater@users.noreply.github.com>
2026-02-23 13:07:49 -0800

    Worked on parsing the negative pnl, and normalized headers for SQL. Next is creating a DB for inserting csv quantities

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,33 @@
+import csv
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
+# ====== CSV PARSING ======
+# removes all the parenthesis and unwanted symbols for neg pnl
+def parseAmount(s):
+    s = s.strip()
+    neg = "(" in s and ")" in s
+    s = s.replace("$", "").replace("(", "").replace(")", "").replace(",", "")
+    val = float(s)
+    return -val if neg else val
+
+with open('./test_data.csv', 'r',) as performance:
+    dt = csv.DictReader(performance)
+
+# Normalizes the headers for clarity
+    up_dt = []
+    for r in dt:
+        print(r)
+        row = {'Symbol': r['symbol'],
+           'Contrace Amount': r['_priceFormat'],
+           'Price Format Type': r['_priceFormatType'],
+           'Tick Size': r['_tickSize'],
+           'pnl': parseAmount(r['pnl']),
+           'Bought Time': r['boughtTimestamp'],
+           'Sold Time': r['soldTimestamp'],
+           'Duration': r['duration']}
+    up_dt.append(row)
+print(up_dt)
+
+# ====== CREATING A DATABASE ======
+conn = sqlite3.connect('trades.db')
+trades = conn.cursor()
